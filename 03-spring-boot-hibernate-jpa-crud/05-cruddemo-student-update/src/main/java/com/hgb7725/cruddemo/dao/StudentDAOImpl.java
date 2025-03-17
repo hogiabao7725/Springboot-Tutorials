2dc1fdc8cd5857b8932fb333c25341d08783e34a
hogiabao <hogiabao7725@gmail.com>
2025-03-17 16:33:50 +0700

    First

---
+++ 03-spring-boot-hibernate-jpa-crud/05-cruddemo-student-update/src/main/java/com/hgb7725/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,61 @@
+package com.hgb7725.cruddemo.dao;
+
+import com.hgb7725.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student ORDER BY lastName", Student.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        // TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName = :lastName", Student.class);
+        // query.setParameter("theData", lastName);
+
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName LIKE :theData", Student.class);
+        query.setParameter("theData", "%" + lastName + "%");
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void updateStudent(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Integer id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/07-cruddemo-create-db-tables-automatically/src/main/java/com/hgb7725/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,23 @@
+package com.hgb7725.cruddemo.dao;
+
+import com.hgb7725.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student student);
+
+    void deleteById(Integer id);
+
+    int deleteAll();
+
+}
